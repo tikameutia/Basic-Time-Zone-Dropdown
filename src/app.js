362b77182609f44fe928aa48091abbadd3b45ec6
Tika Meutia Lubis <111536774+tikameutia@users.noreply.github.com>
2026-05-17 22:02:13 +0200

    Created the function to show local time in each city using moment

--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
 function showLocalTime(event) {
   if (event.target.value.length > 0) {
-    alert("this is the local time now");
+    if (event.target.value === "paris") {
+      let parisLocalTime = moment()
+        .tz("Europe/Paris")
+        .format("dddd, MMMM D, YYYY h:mm A");
+      alert(`It is ${parisLocalTime} in Europe/Paris`);
+    }
+    if (event.target.value === "tokyo") {
+      let tokyoLocalTime = moment()
+        .tz("Asia/Tokyo")
+        .format("dddd, MMMM D, YYYY h:mm A");
+      alert(`It is ${tokyoLocalTime} in Asia/Tokyo`);
+    }
+    if (event.target.value === "sydney") {
+      let sydneyLocalTime = moment()
+        .tz("Australia/Sydney")
+        .format("dddd, MMMM D, YYYY h:mm A");
+      alert(`It is ${sydneyLocalTime} in Australia, Sydney`);
+    }
   }
 }
 
