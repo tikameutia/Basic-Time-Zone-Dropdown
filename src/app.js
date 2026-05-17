450ebb4b6a41478136c59875f5b694f0d03e7940
Tika Meutia Lubis <111536774+tikameutia@users.noreply.github.com>
2026-05-17 22:22:28 +0200

    Refactoring the function to show local time

--- src/app.js
+++ src/app.js
@@ -1,23 +1,9 @@
 function showLocalTime(event) {
   if (event.target.value.length > 0) {
-    if (event.target.value === "paris") {
-      let parisLocalTime = moment()
-        .tz("Europe/Paris")
-        .format("dddd, MMMM D, YYYY h:mm A");
-      alert(`It is ${parisLocalTime} in Europe/Paris`);
-    }
-    if (event.target.value === "tokyo") {
-      let tokyoLocalTime = moment()
-        .tz("Asia/Tokyo")
-        .format("dddd, MMMM D, YYYY h:mm A");
-      alert(`It is ${tokyoLocalTime} in Asia/Tokyo`);
-    }
-    if (event.target.value === "sydney") {
-      let sydneyLocalTime = moment()
-        .tz("Australia/Sydney")
-        .format("dddd, MMMM D, YYYY h:mm A");
-      alert(`It is ${sydneyLocalTime} in Australia, Sydney`);
-    }
+    let LocalTime = moment()
+      .tz(event.target.value)
+      .format("dddd, MMMM D, YYYY h:mm A");
+    alert(`It is ${LocalTime} in ${event.target.value}`);
   }
 }
 
